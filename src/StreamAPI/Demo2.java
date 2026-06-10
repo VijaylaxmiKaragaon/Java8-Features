d926a3b5783a272d072bba08ea38da9a37f61f60
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-06-10 14:01:15 +0530

    initial commit

--- src/StreamAPI/Demo2.java
+++ src/StreamAPI/Demo2.java
@@ -16,8 +16,6 @@ import java.util.Set;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
-import com.collection.Students;
-
 class Emp {
         Integer eid;
         String fname;
